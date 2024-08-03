4c9d79398e5e43bac5c3e2f32dc37836407cc67e
regularpoe <vedranvinko@protonmail.com>
2024-08-03 13:19:26 +0200

    Add handler and monitor fns

--- src/mite/core.clj
+++ src/mite/core.clj
@@ -1,11 +1,29 @@
 (ns mite.core
-  (:require [cheshire.core :as json])
+  (:require [cheshire.core :as json]
+            [hawk.core :as hawk])
   (:gen-class))
 
 (defn read-config [file]
   (let [content (slurp file)]
     (json/parse-string content true)))
 
+(defn handle-event [ctx event]
+  (let [kind (event :kind)
+        path (event :file)]
+    (println (str "Event: " kind " - " path)))
+  ctx)
+
+(defn monitor-paths [paths]
+  (hawk/watch! [{:paths paths
+                 :handler handle-event}]))
+
 (defn -main [& args]
-  (let [config (read-config "config.json")]
-    (println "Config:" config)))
+  (let [config (read-config "config.json")
+        paths (:paths config)
+        poll-interval (:poll_interval config)]
+    (println "Monitoring paths:" paths "with poll interval:" poll-interval)
+    (monitor-paths paths)
+    (loop []
+      (Thread/sleep (* poll-interval 1000))
+      (recur))))
+
