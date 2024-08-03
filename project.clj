38157077dca6974f47e9ec5162679f2052a78740
regularpoe <vedranvinko@protonmail.com>
2024-08-03 21:33:07 +0200

    Add deps for ring/jetty

--- project.clj
+++ project.clj
@@ -4,10 +4,12 @@
   :license {:name "GNU GENERAL PUBLIC LICENSE Version 3"
             :url ""}
   :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/core.async "1.6.681"]
                  [org.clojure/tools.namespace "1.5.0"]
                  [cheshire "5.13.0"]
                  [clj-time "0.15.2"]
-                 [hawk "0.2.11"]]
+                 [hawk "0.2.11"]
+                 [ring/ring-jetty-adapter "1.12.2"]]
   :main ^:skip-aot mite.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
